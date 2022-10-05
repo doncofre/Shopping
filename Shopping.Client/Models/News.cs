59bf02fcf6b2173b67df95c31bd9f9deb8e09d8f
Lucas Cofre <cofre.le@gmail.com>
2022-10-04 23:30:21 -0300

    news section

---
+++ Shopping.Client/Models/News.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Client.Models
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string New { get; set; }
+        public News()
+        {
+
+        }
+
+        // home section will have an amount of 10 news selected by a select top 10
+    }
+}
